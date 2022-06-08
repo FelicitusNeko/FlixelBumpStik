d8e4582d42c73282446394502e599a528adba8be
Felix <felicitusneko@vivaldi.net>
2022-06-08 19:13:38 -0300

    fsm now tracks when state changes

--- source/FSM.hx
+++ source/FSM.hx
@@ -5,19 +5,43 @@ package;
 **/
 class FSM
 {
-	public var activeState:Null<Float->Void>;
+	/** The current active state for this state machine. **/
+	public var activeState(default, set):Null<Float->Void>;
+
+	/** Whether this state machine just changed states in the last frame. **/
+	public var justChanged(default, null) = true;
+
+	/** Whether this state machine has just received a change command during the current frame. **/
+	private var _justChanging = true;
 
 	public function new(initialState:Null<Float->Void> = null)
 	{
 		activeState = initialState;
 	}
 
-	public function update(elapsed:Float)
+	function set_activeState(activeState:Null<Float->Void>):Null<Float->Void>
 	{
-		if (activeState != null)
-			activeState(elapsed);
+		// trace("state change");
+		_justChanging = true;
+		return this.activeState = activeState;
 	}
 
+	/** Calls the update function in this state machine, if any. **/
+	public function update(elapsed:Float)
+	{
+		// if (justChanged)
+		// 	trace("calling new state for first frame");
+		if (activeState != null)
+			activeState(elapsed);
+		justChanged = _justChanging;
+		_justChanging = false;
+	}
+
+	/**
+		Determines whether the current stored state function is the one provided.
+		@param compare The state function to compare against.
+		@return Whether the provided state function (or lack thereof) is the current active function.
+	**/
 	public function is(compare:Null<Float->Void>)
 	{
 		return activeState == compare;
