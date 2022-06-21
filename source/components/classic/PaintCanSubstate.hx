00933e127ae864f2fa65336370700234532351d4
Felix <felicitusneko@vivaldi.net>
2022-06-21 19:31:27 -0300

    use FlxSpriteButton for cancel graphic for some reason, wasn't able to do it just with mouse events

--- source/components/classic/PaintCanSubstate.hx
+++ source/components/classic/PaintCanSubstate.hx
@@ -2,12 +2,14 @@ package components.classic;
 
 import boardObject.Bumper;
 import flixel.FlxG;
-import flixel.FlxSprite;
 import flixel.FlxSubState;
 import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.math.FlxPoint;
+import flixel.text.FlxText;
+import flixel.ui.FlxSpriteButton;
 import flixel.util.FlxColor;
 import flixel.util.FlxSpriteUtil;
+import lime.app.Event;
 
 class PaintCanSubstate extends FlxSubState
 {
@@ -16,6 +18,8 @@ class PaintCanSubstate extends FlxSubState
 	public var colors(default, null):Int;
 
 	public var center(default, null):FlxPoint;
+
+	public var onDialogResult(default, null) = new Event<Color->Void>();
 
 	public function new(center:FlxPoint, colors = 3)
 	{
@@ -29,32 +33,40 @@ class PaintCanSubstate extends FlxSubState
 		super.create();
 
 		var distanceFactor = 1.3;
-
 		var vertices:Array<FlxPoint> = [];
+		var bumperHeight:Float = -1;
 
-		var hex = new FlxSprite(center.x - 40, center.y - 40);
-		hex.makeGraphic(144, 144, FlxColor.TRANSPARENT);
+		var hexbutton = new FlxSpriteButton(center.x + 70, center.y + 40, null, () -> onDialogResult.dispatch(None));
+		hexbutton.makeGraphic(144, 144, FlxColor.TRANSPARENT);
 
 		for (i => color in BumperGenerator.colorOpts)
 		{
 			var bumper = new Bumper(center.x, center.y, color, i < colors ? Up : None);
-			bumper.x += Math.cos(DegRad(-90 + ((i - 1) * 60))) * (bumper.height * distanceFactor);
-			bumper.y += Math.sin(DegRad(-90 + ((i - 1) * 60))) * (bumper.height * distanceFactor);
+			if (bumperHeight < 0)
+				bumperHeight = bumper.height;
+			bumper.x += Math.cos(DegRad(-90 + ((i - 1) * 60))) * (bumperHeight * distanceFactor);
+			bumper.y += Math.sin(DegRad(-90 + ((i - 1) * 60))) * (bumperHeight * distanceFactor);
 			bumper.grayedOut = i >= colors;
 			bumper.angle = (i - 1) * 60;
 			_bumpers.add(bumper);
 
-			// TODO: don't hardcode things
-			vertices.push(new FlxPoint(Math.cos(DegRad(((i - 1) * 60))) * (bumper.height * (distanceFactor - .1)),
-				Math.sin(DegRad(((i - 1) * 60))) * (bumper.height * (distanceFactor - .1))).add(72, 72));
+			vertices.push(new FlxPoint(Math.cos(DegRad((i - 1) * 60)), Math.sin(DegRad((i - 1) * 60))));
 		}
 
-		trace(vertices);
+		for (vertex in vertices)
+			vertex.scale(bumperHeight).add(hexbutton.width / 2, hexbutton.height / 2);
 
-		FlxSpriteUtil.drawPolygon(hex, vertices, FlxColor.WHITE, {thickness: 3}, {smoothing: true});
-		add(hex);
+		FlxSpriteUtil.drawPolygon(hexbutton, vertices, FlxColor.BLACK, {thickness: 3, color: FlxColor.RED}, {smoothing: true});
+		add(hexbutton);
+
+		var cancelText = new FlxText(center.x + (bumperHeight / 2) - 36, center.y + (bumperHeight / 2), 72, "Cancel", 16);
+		cancelText.alignment = CENTER;
+		cancelText.y -= cancelText.height / 2;
+		add(cancelText);
 
 		add(_bumpers);
+
+		onDialogResult.add(onDialogResultF);
 	}
 
 	override function update(elapsed:Float)
@@ -65,9 +77,17 @@ class PaintCanSubstate extends FlxSubState
 		{
 			var pos = FlxG.mouse.getWorldPosition();
 			for (bumper in _bumpers)
-				if (bumper.overlapsPoint(pos))
-					trace(bumper);
+				if (bumper.overlapsPoint(pos) && !bumper.grayedOut)
+				{
+					onDialogResult.dispatch(bumper.bColor);
+					return;
+				}
 		}
+	}
+
+	function onDialogResultF(_:Color)
+	{
+		close();
 	}
 
 	inline function DegRad(deg:Float):Float
