2ec32d477c0c5fefac0e2c865490caf0ae849e37
Felix <felicitusneko@vivaldi.net>
2022-06-27 18:29:37 -0300

    make Paint Can substate darken main scene

--- source/components/classic/PaintCanSubstate.hx
+++ source/components/classic/PaintCanSubstate.hx
@@ -26,7 +26,7 @@ class PaintCanSubstate extends FlxSubState
 	{
 		this.center = center;
 		this.colors = colors;
-		super();
+		super(FlxColor.fromRGBFloat(0, 0, 0, .3));
 	}
 
 	override function create()
