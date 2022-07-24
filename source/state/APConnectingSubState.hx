dc7e030774a982b209be00e64f05bf63e0c3ccf2
Felix R <felicitusneko@duck.com>
2022-07-23 23:23:45 -0300

    add modals for APEntryState

---
+++ source/state/APConnectingSubState.hx
@@ -0,0 +1,34 @@
+package state;
+
+import flixel.FlxSprite;
+import flixel.FlxSubState;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+
+using flixel.util.FlxSpriteUtil;
+
+class APConnectingSubState extends FlxSubState
+{
+	public function new()
+	{
+		super(FlxColor.fromRGBFloat(0, 0, 0, .5));
+	}
+
+	override function create()
+	{
+		var connectingText = new FlxText(0, 0, 0, "Connecting...", 20);
+		connectingText.color = FlxColor.WHITE;
+
+		var backdrop = new FlxSprite(-11, -11);
+		backdrop.makeGraphic(Math.round(connectingText.width + 22), Math.round(connectingText.height + 22), FlxColor.TRANSPARENT);
+		backdrop.drawRoundRect(1, 1, backdrop.width, backdrop.height, 20, 20, FlxColor.BLACK, {color: FlxColor.WHITE, thickness: 3});
+
+		add(backdrop);
+		add(connectingText);
+
+		for (item in [connectingText, backdrop])
+			item.screenCenter();
+
+		super.create();
+	}
+}
---
+++ source/state/APErrorSubState.hx
@@ -0,0 +1,55 @@
+package state;
+
+import flixel.FlxSprite;
+import flixel.FlxSubState;
+import flixel.text.FlxText;
+import flixel.ui.FlxButton;
+import flixel.util.FlxColor;
+
+using flixel.util.FlxSpriteUtil;
+
+class APErrorSubState extends FlxSubState
+{
+	public var message(default, null):String;
+
+	public function new(message:String)
+	{
+		this.message = message;
+		super(FlxColor.fromRGBFloat(0, 0, 0, 0.5));
+	}
+
+	override function create()
+	{
+		var errorText = new FlxText(0, 0, 0, "Error", 20);
+		errorText.color = FlxColor.fromRGBFloat(1, .5, .5);
+
+		var messageText = new FlxText(0, 0, 0, message, 12);
+		messageText.fieldWidth = Math.min(messageText.fieldWidth, 400);
+		messageText.color = FlxColor.WHITE;
+		messageText.alignment = CENTER;
+
+		var closeButton = new FlxButton(0, 0, "Close", close);
+
+		var backdrop = new FlxSprite(-11, -11);
+		backdrop.makeGraphic(Math.round(Math.max(errorText.width, messageText.width) + 22),
+			Math.round(errorText.height + messageText.height + closeButton.height + 40), FlxColor.TRANSPARENT);
+		backdrop.drawRoundRect(1, 1, backdrop.width - 2, backdrop.height - 2, 20, 20, FlxColor.BLACK, {color: FlxColor.WHITE, thickness: 3});
+
+		backdrop.screenCenter();
+		for (item in [errorText, messageText, closeButton])
+			item.screenCenter(X);
+
+		errorText.y = backdrop.y + 10;
+		messageText.y = errorText.y + errorText.height + 10;
+		closeButton.y = messageText.y + messageText.height + 10;
+
+		for (item in [backdrop, errorText, messageText, closeButton])
+		{
+			item.x = Math.round(item.x);
+			item.y = Math.round(item.y);
+			add(item);
+		}
+
+		super.create();
+	}
+}
