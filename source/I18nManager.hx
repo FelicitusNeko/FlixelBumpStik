cb36c7937298b427b14f0df7067ef75615b89682
Felix R <felicitusneko@duck.com>
2022-08-08 23:32:20 -0300

    add I18nManager as jsoni18n wrapper

---
+++ source/I18nManager.hx
@@ -0,0 +1,33 @@
+package;
+
+import haxe.Exception;
+import jsoni18n.I18n;
+import openfl.utils.Assets;
+
+class I18nManager
+{
+	public static var defaultLang = "en-US";
+
+	private var _i18n = new I18n();
+
+	public var lang(default, set):String;
+
+	public var tr(default, null):(String, ?Map<String, String>) -> String;
+
+	public function new(?lang:String)
+	{
+		tr = _i18n.tr;
+		this.lang = lang == null ? defaultLang : lang;
+	}
+
+	private function set_lang(lang:String)
+	{
+		if (!Assets.exists('assets/i18n/$lang.json'))
+			throw new Exception('Language file $lang.json not found');
+		_i18n.clear();
+		_i18n.loadFromString(Assets.getText('assets/i18n/$defaultLang.json'));
+		if (lang != defaultLang)
+			_i18n.loadFromString(Assets.getText('assets/i18n/$lang.json'));
+		return this.lang = lang;
+	}
+}
