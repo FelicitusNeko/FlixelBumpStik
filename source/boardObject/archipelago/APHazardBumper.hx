f41588e6e0a65f6a2f6c6d80a82147fbdf0bb4a2
Felix R <felicitusneko@duck.com>
2022-08-17 17:43:42 -0300

    make it more obvious when hazard becomes clearable

--- source/boardObject/archipelago/APHazardBumper.hx
+++ source/boardObject/archipelago/APHazardBumper.hx
@@ -2,11 +2,16 @@ package boardObject.archipelago;
 
 import components.Board;
 import flixel.FlxSprite;
+import flixel.tweens.FlxEase;
 import flixel.util.FlxColor;
+
+using flixel.tweens.FlxTween;
 
 class APHazardBumper extends Bumper
 {
 	private var _resolveColor:FlxColor;
+
+	private var _flashCount:Float = 0;
 
 	public function new(x:Float, y:Float, resolveColor:FlxColor, ?owner:Board, turnsToNormal = 5)
 	{
@@ -25,12 +30,25 @@ class APHazardBumper extends Bumper
 			if (--health <= 0)
 			{
 				var hazardFlair = _flairList["hazard"];
-				hazardFlair.color = FlxColor.fromRGBFloat(1, 1, 1, .5);
+				hazardFlair.tween({alpha: 0, "scale.x": 1.25, "scale.y": 1.25}, .5, {ease: FlxEase.circOut});
 				bColor = _resolveColor;
 				return true;
 			}
 		}
 
 		return false;
+	}
+
+	override function update(elapsed:Float)
+	{
+		if (health == 1)
+		{
+			_flashCount += elapsed;
+			while (_flashCount >= .5)
+			{
+				_flashCount -= .5;
+				arrow.color = (arrow.color == _resolveColor) ? base.color : _resolveColor;
+			}
+		}
 	}
 }
