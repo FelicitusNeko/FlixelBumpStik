d37e1483efd2475e3cc2a1866f328fc85eced60e
Felix <felicitusneko@duck.com>
2022-11-23 16:49:28 -0400

    make i18n read-only + docs

--- source/BumpStikGame.hx
+++ source/BumpStikGame.hx
@@ -3,9 +3,11 @@ package;
 import flixel.FlxG;
 import flixel.FlxGame;
 
+/** The Bumper Stickers base game instance. **/
 class BumpStikGame extends FlxGame
 {
-	public var i18n:I18nManager;
+	/** Read-only. The internationalization/strings manager for the game. **/
+	public var i18n(default, null):I18nManager;
 
 	public function new(GameWidth:Int = 0, GameHeight:Int = 0, ?InitialState:Class<flixel.FlxState>, Zoom:Float = 1, UpdateFramerate:Int = 60,
 			DrawFramerate:Int = 60, SkipSplash:Bool = false, StartFullscreen:Bool = false)
@@ -15,9 +17,11 @@ class BumpStikGame extends FlxGame
 		i18n = new I18nManager();
 	}
 
+	/** Casts the current FlxGame to a BumpStikGame. **/
 	public static inline function g()
 		return cast(FlxG.game, BumpStikGame);
 
+	/** Converts degrees to radians. **/
 	public static inline function DegRad(deg:Float):Float
 		return deg * Math.PI / 180;
 }
