52efc7fc9d442bb85e821feffdcb5a1b3945b409
Felix R <felicitusneko@duck.com>
2023-06-20 18:26:12 -0300

    launch into main menu if built final also clean things up so there's only one conditional here

--- source/Main.hx
+++ source/Main.hx
@@ -1,10 +1,12 @@
 package;
 
 import openfl.display.Sprite;
-#if debug
-import state.APEntryState;
+#if final
+import state.MenuState as GoState;
+#elseif debug
+import state.APEntryState as GoState;
 #else
-import components.classic.ClassicGameState;
+import components.classic.ClassicGameState as GoState;
 #end
 
 typedef I18nFunction = (String, ?Map<String, Dynamic>) -> String;
@@ -14,10 +16,6 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		#if debug
-		addChild(new BumpStikGame(0, 0, APEntryState));
-		#else
-		addChild(new BumpStikGame(0, 0, ClassicGameState));
-		#end
+		addChild(new BumpStikGame(0, 0, GoState));
 	}
 }
