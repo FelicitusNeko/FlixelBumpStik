c682e2114e732522384549ed4704843a9ccb58ba
Felix R <felicitusneko@duck.com>
2024-11-18 16:15:09 -0400

    remove disconnect fallbacks these are handled directly by hxArchipelago now

--- source/components/archipelago/BumpStikClient.hx
+++ source/components/archipelago/BumpStikClient.hx
@@ -1,5 +1,6 @@
 package components.archipelago;
 
+import ap.Definitions.OfflineQueueType;
 import ap.Client;
 
 class BumpStikClient extends Client
@@ -8,7 +9,7 @@ class BumpStikClient extends Client
 
 	public var _password:Null<String>;
 
-	public var checkQueue(get, null):Array<Int> = [];
+	public var checkQueue(get, null):Array<OfflineQueueType> = [];
 
 	public function new(slotName:String, game, uri, ?password)
 	{
@@ -16,39 +17,41 @@ class BumpStikClient extends Client
 		_slotName = slotName;
 		_password = password;
 		onRoomInfo.add(onRoomInfoH, -100);
-		onSlotConnected.add(onSlotConnectedH);
+		//onSlotConnected.add(onSlotConnectedH);
 	}
 
+	// public function get_checkQueue()
+	// 	return this.checkQueue.slice(0);
 	public function get_checkQueue()
-		return this.checkQueue.slice(0);
+		return this._offlineQueue.slice(0);
 
-	function onSlotConnectedH(_)
-	{
-		if (_checkQueue.length > 0)
-		{
-			super.LocationChecks(checkQueue);
-			checkQueue = [];
-		}
-	}
+	// function onSlotConnectedH(_)
+	// {
+	// 	if (_offlineQueue.length > 0)
+	// 	{
+	// 		super.LocationChecks(_offlineQueue);
+	// 		_offlineQueue = [];
+	// 	}
+	// }
 
-	function onRoomInfoH()
+	private function onRoomInfoH()
 	{
 		#if debug
 		var tags = ["AP", "Testing"];
 		#else
 		var tags = ["AP"];
 		#end
-		ConnectSlot(_slotName, _password, 0x7, tags, {major: 0, minor: 4, build: 3});
+		ConnectSlot(_slotName, _password, 0x7, tags, {major: 0, minor: 5, build: 0});
 	}
 
-	public override function LocationChecks(locations:Array<Int>):Bool
-	{
-		if (state == SLOT_CONNECTED)
-			return super.LocationChecks(locations);
-		else
-		{
-			checkQueue = checkQueue.concat(locations);
-			return false;
-		}
-	}
+	// public override function LocationChecks(locations:Array<Int>):Bool
+	// {
+	// 	if (state == SLOT_CONNECTED)
+	// 		return super.LocationChecks(locations);
+	// 	else
+	// 	{
+	// 		checkQueue = checkQueue.concat(locations);
+	// 		return false;
+	// 	}
+	// }
 }
