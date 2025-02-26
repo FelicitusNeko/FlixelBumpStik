b7f005c49941409869cde6314b12f0a75b57982d
Felix R <felicitusneko@duck.com>
2025-02-26 16:09:08 -0400

    add (commented) defines to Project.xml

--- source/BumpStikGame.hx
+++ source/BumpStikGame.hx
@@ -8,7 +8,7 @@ import flixel.FlxState;
 class BumpStikGame extends FlxGame
 {
 	/** _Read-only._ The current save version. Earlier saves than this may be converted or deleted. **/
-	#if debug
+	#if nosave
 	public static final curSaveVer = -1;
 	#else
 	public static final curSaveVer = 2;
